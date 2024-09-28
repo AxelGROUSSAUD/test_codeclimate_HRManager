8f2a1951010a589bbf9c41e40fd23a9fa0a9eb7d
AxelGROUSSAUD <groussaudaxelpro@gmail.com>
2024-09-28 16:35:42 +0200

    another test for deepsource 2

--- src/main/resources/stop_services.sh
+++ src/main/resources/stop_services.sh
@@ -5,26 +5,26 @@ for i in "$@"
 do
 
   #Affiche le paramètre renseigner (Debug)
-  echo $i
+  echo "$i"
 
   #Declare une variable qui récupère l'id du container
   id_container=$(podman ps -a -q --filter "name=$i" | head -n 1)
 
   #Affiche l'id du container (Debug)
-  echo $id_container
+  echo "$id_container"
   
   #Si on trouve un id d'un container
   if [ ${#id_container} -ne 0 ]; then
   
     #Récupère le status du container en fonction de son id
-    status_container=$(podman inspect $id_container --format '{{.State.Status}}')
+    status_container=$(podman inspect "$id_container" --format '{{.State.Status}}')
 
     #Affiche le status du container (Debug)
-    echo $status_container
+    echo "$status_container"
 
     # si le container n'est pas lancé alors on le démarre
-    if [ $status_container != "exited" ]; then
-      podman stop $id_container
+    if [ "$status_container" != "exited" ]; then
+      podman stop "$id_container"
     fi
   
   else
